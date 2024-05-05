6b547fd7222d2591ac753dc695f488db745920d3
Taha0229 <sheikhtahamaroof@gmail.com>
2024-05-06 04:17:13 +0530

    Style-project file structure setup

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "UNetR_MC_Full-stack"
+AUTHOR_USER_NAME = "Taha0229"
+SRC_REPO = "UNetR_MultiClass"
+AUTHOR_EMAIL = "sheikhtahamaroof@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A state-of-the-art U-Net architecture powered by Vision Transformer.",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
